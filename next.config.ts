e9a5dceaa0ebdb19cc887ece92918a30a9d9f062
clementeaf <carriagadafalcone@gmail.com>
2025-04-10 08:07:05 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  eslint: {
+    // Ignora los errores de ESLint durante la compilación
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Ignora los errores de TypeScript durante la compilación
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
